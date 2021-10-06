5606dee8f465a1649b54be98d337044268cd992a
Victor Galaschi <victorgalaschi@hotmail.com>
2021-10-06 12:19:39 -0300

    ajustes para subir no heroku

--- envio_mensagem_whatsapp/__init__.py
+++ envio_mensagem_whatsapp/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import os
 
+os.chmod('envio_mensagem_whatsapp', 755)
 app = Flask(__name__)
 
 from envio_mensagem_whatsapp import routes
--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from envio_mensagem_whatsapp import app
 
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
-
-
